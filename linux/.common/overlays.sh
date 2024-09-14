b693d5b8536b7ca18f559c0130f3e94a76d43244
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-09-14 16:33:10 +0530

    bsp: Add IMX708 support
    Change-Id: I8a516e4efcbf207502f3d0560f4be33b82795ef0
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- linux/.common/overlays.sh
+++ linux/.common/overlays.sh
@@ -1,4 +1,4 @@
 custom_source_action() {
-    git_source https://github.com/radxa-pkg/radxa-overlays.git 04de4cab2c5eba53cb2caff940b78822dac50ab5
+    git_source https://github.com/UtsavBalar1231/radxa-overlays.git 1a9812085d2090333ede4b10d990e5242109deb0
     cp -r $SCRIPT_DIR/.src/radxa-overlays/arch $TARGET_DIR
 }
